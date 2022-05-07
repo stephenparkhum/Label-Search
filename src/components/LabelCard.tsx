d934c6faa461a742fb2c0a16a4d8d2d449295fd2
SP <stephenparkhum@outlook.com>
2022-05-06 21:05:55 -0700

    Dark stuff

--- src/components/LabelCard.tsx
+++ src/components/LabelCard.tsx
@@ -3,12 +3,17 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { LabelCardProps } from '../types/types';
-import EmailIcon from '@mui/icons-material/Email';
-import LanguageIcon from '@mui/icons-material/Language';
 import { Link, Box } from '@mui/material';
+
+// Icons
+import EmailIcon from '@mui/icons-material/Email';
+import CheckIcon from '@mui/icons-material/Check';
+import LanguageIcon from '@mui/icons-material/Language';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const LabelCard: FC<LabelCardProps> = ({ label }): ReactElement => {
   const { genres } = label;
+  const mGap = 1.5;
 
   const displayLabelGenres = (genres: string[]) => {
     return genres.map((genre: string) => {
@@ -17,13 +22,13 @@ const LabelCard: FC<LabelCardProps> = ({ label }): ReactElement => {
   };
 
   return (
-    <Card sx={{ mb: 1.5, minWidth: 275 }}>
+    <Card sx={{ mb: mGap, minWidth: 275 }}>
       <CardContent>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: mGap }}>
+          <Typography sx={{ fontSize: 22 }} color="text.primary" gutterBottom>
             {label.displayName}
           </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          <Typography color="text.secondary">
             <Link
               href={`${label.email.includes('@') ? 'mailto:' : ''}${label.email}`}
               target="blank">
@@ -32,10 +37,19 @@ const LabelCard: FC<LabelCardProps> = ({ label }): ReactElement => {
             </Link>
           </Typography>
         </Box>
-        <Typography variant="body2">Genres:</Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {displayLabelGenres(genres)}
-        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography variant="body1" component="p">
+            Accepting Demos?
+          </Typography>
+          {!label.accepting && <CancelIcon sx={{ ml: mGap }} color="error" />}
+          {label.accepting && <CheckIcon sx={{ ml: mGap }} color="success" />}
+        </Box>
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="body2">Genres:</Typography>
+          <Typography sx={{ mb: mGap }} color="text.secondary">
+            {displayLabelGenres(genres)}
+          </Typography>
+        </Box>
       </CardContent>
     </Card>
   );
